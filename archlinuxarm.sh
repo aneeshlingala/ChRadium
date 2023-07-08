1b5167dfa7b37955311ef3a0d2296c7c0c3364b3
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-07-07 22:05:46 -0700

    more thing

--- archlinuxarm.sh
+++ archlinuxarm.sh
@@ -1,4 +1,4 @@
-echo "Arch Linux ARM or blendOS for unsupported Chromebooks"
+echo "Arch Linux ARM for unsupported Chromebooks"
 echo "Release 2023.07.07, Pani Puri"
 echo ""
 
@@ -22,10 +22,10 @@ else
 fi
 
 TARGET=$(echo $2)
-disk_size=$(sudo blockdev --getsize64 "/dev/$TARGET")
-sector_size=$(sudo blockdev --getss "/dev/$TARGET")
+disk_size=$(blockdev --getsize64 "/dev/$TARGET")
+sector_size=$(blockdev --getss "/dev/$TARGET")
 total_sectors=$((disk_size / sector_size))
-start_sector=$(sudo cgpt show -i 1 -b "/dev/$TARGET")
+start_sector=$(cgpt show -i 1 -b "/dev/$TARGET")
 end_sector=$((total_sectors - 1))
 
 
@@ -42,7 +42,12 @@ if [ "$1" == "--device=kukui" ]; then
      cgpt create /dev/$(echo $TARGET)
      cgpt add -i 1 -t kernel -b 8192 -s 65536 -l Kernel -S 1 -T 5 -P 10 /dev/$(echo $TARGET)
      cgpt add -i 2 -t data -s "$start_sector" -e "$end_sector" -l Root "/dev/$TARGET"
-     mkfs.ext4 "/dev/$(echo $TARGET)"
+     if [[ $TARGET =~ mmcblk[01] ]]; then
+        mkfs.ext4 "/dev/$(echo $TARGET)p2"
+        
+    elif [[ -b $disk_node ]]; then
+        mkfs.ext4 "/dev/$(echo $TARGET)2"
+    fi
      echo "Downloading RootFS, this may take a while..."
      cd /tmp
      wget http://os.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz
@@ -51,7 +56,16 @@ if [ "$1" == "--device=kukui" ]; then
         mount /dev/$(echo $TARGET)p2 /tmp/tmpmount
         
     elif [[ -b $disk_node ]]; then
+        mkdir /tmp/tmpmount
         mount /dev/$(echo $TARGET)2 /tmp/tmpmount
     fi
-       
+    bsdtar -xpf ArchLinuxARM-aarch64-latest.tar.gz -C /tmp/tmpmount
+    mount -t devtmpfs /dev /tmp/tmpmount/dev
+    mount -o bind /proc /tmp/tmpmount/proc
+    mount -o bind /run /tmp/tmpmount/run
+    mount -o bind /sys /tmp/tmpmount/sys
+    rm -rf /tmp/tmpmount/etc/resolv.conf
+    cp /etc/resolv.conf /tmp/tmpmount/etc/
+    cp chroot-run.sh /tmp/tmpmount/
+    chroot /tmp/tmpmount/chroot-run.sh /bin/bash -c "su - -c /chroot-run.sh"
  fi
