0b678a9bd02a6f6695f0be41f6bb0b79af36b408
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-07-18 20:38:27 -0700

    Create chroot-run-debian.sh

---
+++ chroot-run-debian.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "Starting chroot script..."
+userdel linux
+rm -rf /home/linux
+
+read -p "Enter the username to add: " username
+useradd "$username"
+passwd "$username"
+mkdir /home/$(echo $username)
+
+if [[ $? -eq 0 ]]; then
+  echo "User '$username' created successfully."
+else
+  echo "Error: Failed to create user '$username'."
+fi
+
+read -p "Enter the new hostname: " new_hostname
+
+hostnamectl set-hostname "$new_hostname"
+
+if [[ $? -eq 0 ]]; then
+  echo "Hostname changed to '$new_hostname' successfully."
+else
+  echo "Error: Failed to change hostname to '$new_hostname'."
+fi
+
+echo "$(echo $username) ALL=(ALL:ALL) ALL" | tee -a /etc/sudoers
+
+echo "Enter password to set for root"
+passwd
+
+read -p "Do you want the Xfce desktop environment installed? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Nn]$ ]]
+then
+    echo "Removing Xfce..."
+    apt purge xfconf xfce4-utils xfwm4 xfce4-session xfdesktop4 exo-utils xfce4-panel xfce4-terminal thunar -y
+    apt remove xfce4* -y
+    apt autoremove -y
+fi
